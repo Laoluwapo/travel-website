8749459c96f91af3d143e6a53762850394b0922e
laoluwapo <alividegabriel9@gmail.com>
2023-03-13 20:32:00 +0100

    Add animations on toggle to the burger.

--- app.js
+++ app.js
@@ -64,8 +64,9 @@ function animateSlides() {
   });
 }
 
-let mouse = document.querySelector(".cursor");
-let mouseTxt = mouse.querySelector("span");
+const mouse = document.querySelector(".cursor");
+const mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 // Function that adds the cursor div to the main cursor
 function cursor(e) {
   mouse.style.top = e.pageY + "px";
@@ -91,7 +92,32 @@ function activeCursor(e) {
   }
 }
 
+// Function to toggle the nav
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, {
+      clipPath: "circle(2500px at 100% -10%",
+    });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, {
+      clipPath: "circle(50px at 100% -10%",
+    });
+    document.body.classList.remove("hide");
+  }
+}
+
 // Event listeners
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
+
 animateSlides();
